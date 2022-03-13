3b2c0184cf8e61adf2b8d67770960f24234c802f
hugofpaiva <hello@hugopaiva.com>
2022-03-13 12:05:21 +0000

    feature: line plot for each column

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
+import pandas as pd
+import csv
+import argparse
 import sys
+import plotly.express as px
 
-def hello(name):
-  """Print "Hello " and a name and return None"""
-  print("Hello", name)
+
+def openCSVFile(pathName):
+    file = open(pathName)
+    reader = csv.reader(file, delimiter=',')
+    return reader
+
+
+def plotLineChart(columnName, data):
+    df = pd.DataFrame(dict(
+        x=[i for i in range(1, len(data)+1)],
+        y=data
+    ))
+    fig = px.line(df, x="x", y="y", title=f"Test {columnName}")
+    fig.show()
+
 
 if __name__ == '__main__':
-  program = sys.argv[0]
-  hello('World')
-  print("Program running is:", program)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv_path",
+                        help="CSV File Path", default="./dev.csv")
+    args = parser.parse_args()
+
+    reader = openCSVFile(args.csv_path)
+
+    dataList = []
+
+    for row in reader:
+        for index, column in enumerate(row):
+            if index >= len(dataList):
+                dataList.append([])
+            dataList[index].append(column)
+
+    for index, column in enumerate(dataList):
+        if index == 7:
+            plotLineChart(index+1, column)
+
+
+
